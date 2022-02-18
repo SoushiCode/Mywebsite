f47831ce09dadce0147621f961cd39d556ab0f86
SoushiCode <soroushmansouri@gmail.com>
2022-02-18 21:44:29 +0330

    created first app
    created first app named website with three views for contact, about and index

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from website.views import about_view, contact_view, index_view
+
+urlpatterns = [
+    path('home', index_view),
+    path('contact',contact_view),
+    path('about',about_view)
+
+]
