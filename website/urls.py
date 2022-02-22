6f89e4807d218c614c0a5b1efe2a4e5b7183f39f
SoushiCode <soroushmansouri@gmail.com>
2022-02-22 15:37:56 +0330

    Make original templates and static addresses
    -Delete initiate templates
    +Add new templates
    +Change addresses to static address in all templates
    +Add a base page and forward header and footer in
    +Add a new app name blog and add url of that

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from website.views import about_view, contact_view, index_view
-
+from website.views import *
+app_name='website'
 urlpatterns = [
-    path('', index_view),
-    path('contact',contact_view),
-    path('about',about_view)
+    path('', index_view,name='index'),
+    path('contact',contact_view,name='contact'),
+    path('about',about_view,name='about')
 
 ]
